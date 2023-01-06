7e077975f582f50cddfb78714c88921127eb9f4a
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2023-01-06 14:04:52 +0800

    Update Problem 25.cpp

--- 20-29/Problem 25.cpp
+++ 20-29/Problem 25.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main() {
+  string prev = "1";
+  string curr = "1";
+
+  int index = 2; // The first two Fibonacci numbers are already accounted for
+  while (curr.size() < 1000) {
+    string next = curr;
+    curr += prev;
+    prev = next;
+    ++index;
+  }
+
+  cout << index << endl;
+  return 0;
+}
